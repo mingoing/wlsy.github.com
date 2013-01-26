584a52524994f90f80781ecd9f111a0d038b9a5d
wlsy <wlsy@wlsys-MacBook-Pro.local>
2013-01-27 00:22:59 +0800

    daily backup

--- static/pizza.js
+++ static/pizza.js
@@ -16,8 +16,28 @@
             username: 'wlsy638',
             albumid: '5834489861667785793',
             picasaServer: 'picasaweb.google.com',
+            perPageResults: 2,
             papi: undefined
         };
+
+        // get from picasa
+        var arPhoto = {
+            photo: [],
+            total: 0,
+            nowIndex: 0
+        };
+
+        // cache
+        var meta = $('#J_PizzaMeta');
+        var metap = $('#J_PizzaMetap');
+        var img = $('#J_PizzaImg');
+        var photoWrapper = $('#J_PizzaWrapper');
+        var loading = $('#J_PizzaLoading');
+        var photoNext = $('#J_PizzaNext');
+        var photoPrev = $('#J_PizzaPrev');
+        var index = $('#J_PizzaIndex');
+        var total = $('#J_PizzaTotalIndex');
+        var nowIndex = $('#J_PizzaNowIndex');
 
         opts = $.extend(defaultOpts, opts || {});
 
@@ -79,22 +99,48 @@
             }
         }
 
+        /**
+         * get data  
+         */
+
         function getPicasa(config, next) {
+
             if (arguments.length === 1) {
                 next = config;
             }
 
             config = $.extend({
                 kind: 'photo',
-                page: 0,
-                maxResults: 10
-            }, {});
+                page: 0
+            }, config || {});
+
+            console.log(config);
 
             $.ajax({
-                url: opts.papi || 'https://' + opts.picasaServer + '/data/feed/api/user/' + opts.username + '/albumid/' + opts.albumid + '?kind=' + config.kind + '&alt=json&start-index=' + (config.page * config.maxResults + 1) + '&max-results=' + config.maxResults,
+                url: opts.papi || 'https://' + opts.picasaServer + '/data/feed/api/user/' + opts.username + '/albumid/' + opts.albumid + '?kind=' + config.kind + '&alt=json&start-index=' + (config.page * opts.perPageResults + 1) + '&max-results=' + opts.perPageResults,
                 dataType: 'jsonp',
                 success: function(data) {
-                    next.call(null, data);
+                    $.each(data.feed.entry, function(index, el) {
+                        var p = {};
+                        p.id = el.gphoto$id.$t;
+                        p.size = {
+                            width: el.gphoto$width.$t,
+                            height: el.gphoto$height.$t
+                        };
+                        p.time = el.exif$tags.exif$time.$t;
+                        p.meta = el.summary.$t;
+                        p.tag = el.media$group.media$keywords.$t;
+                        //photo.size = el.gphoto$size;
+                        p.src = el.content.src;
+
+                        arPhoto.photo.push(p);
+                    });
+
+                    arPhoto.total = data.feed.openSearch$totalResults.$t;
+                    arPhoto.startIndex = data.feed.openSearch$startIndex.$t;
+                    arPhoto.itemsPerPage = data.feed.openSearch$itemsPerPage.$t;
+
+                    next.call(null, arPhoto);
                 }
             });
         }
@@ -116,37 +162,140 @@
             return s.join('/');
         }
 
-        //cache
-        var meta = $('#J_Meta');
-        var metap = $('#J_Metap');
-        var img = $('#J_Img');
-        var photoWrapper = $('#J_PhotoWrapper');
+        /**
+         * 禁用控制按钮  
+         *
+         * @api private
+         */
+
+        function controlDisabled() {
+            switch (arPhoto.nowIndex) {
+            case 0:
+                photoPrev.addClass('pizzaControlDisabled');
+                break;
+
+            case arPhoto.total - 1:
+                photoNext.addClass('pizzaControlDisabled');
+                break;
+
+            default:
+                photoPrev.removeClass('pizzaControlDisabled');
+                photoNext.removeClass('pizzaControlDisabled');
+            }
+
+        }
+
+        /**
+         * onload   
+         */
+
+        function init(data) {
+            $("<style> .pizzaControlDisabled{ opacity: 0.6; cursor: not-allowed;} </style>").appendTo("head");
+            total.html(data.total);
+            nowIndex.html(data.index);
+            index.fadeIn();
+            controlDisabled();
+        }
 
         var photo = {
-            show: function(entry) {
-                var size = picFillSize(entry.media$group.media$thumbnail[2]);
+            show: function(data, index) {
 
-                photoWrapper.width(size.width).height(size.height).fadeIn('slow', function() {
-                    img.attr('src', resolveSrc(size, entry.content.src));
-                    img.on('load', function() {
-                        img.fadeIn('slow', function() { 
-                            metap.html(entry.summary.$t);
-                            metap.width(size.width - 20);
-                            meta.fadeIn('slow');
-                        });
+                if (arguments.length === 1) {
+                    index = 0;
+                }
+
+                loading.show();
+                data.nowIndex = index;
+
+                if (data.nowIndex === data.photo.length && data.nowIndex < data.total) {
+                    getPicasa({
+                        page: arPhoto.photo.length / arPhoto.itemsPerPage 
+                    }, function(data) { 
+                        console.log(data)
+                        render(data);
                     });
 
-                });
+                } else {
+                    render(data);
+                }
+
+                function render(data) {
+                    var size = picFillSize(data.photo[index].size);
+                    photoWrapper.width(size.width).height(size.height).fadeIn('slow', function() {
+                        img.attr('src', resolveSrc(size, data.photo[index].src));
+
+                        nowIndex.fadeOut('fast', function() {
+                            nowIndex.html(arPhoto.nowIndex + 1);
+                            nowIndex.fadeIn('fast');
+                        });
+
+                        img.on('load', function() {
+                            loading.hide();
+                            img.fadeIn('slow', function() {
+                                metap.html(data.photo[index].meta);
+                                metap.width(size.width - 20);
+                                meta.fadeIn('slow');
+
+                                //update index
+                            });
+                        });
+
+                    });
+                }
+
             },
-            hide: function() { 
-                metap.hide();
+
+            next: function(data, index) {
+
+                if (arguments.length === 1) {
+                    index = data.nowIndex + 1 || 0;
+                }
+
+                if (data.nowIndex === data.total - 1) {
+                    return;
+                }
+
+                this.hide();
+                this.show(data, index);
+
+                controlDisabled();
+            },
+
+            prev: function(data, index) {
+
+                if (arguments.length === 1) {
+                    index = data.nowIndex - 1 || 0;
+                }
+
+                if (data.nowIndex === 0) {
+                    return;
+                }
+
+                this.hide();
+                this.show(data, index);
+
+                controlDisabled();
+            },
+
+            hide: function() {
+                meta.hide();
                 img.hide();
             }
         };
 
+        photoNext.on('click', function(ev) {
+            ev.preventDefault();
+            photo.next(arPhoto);
+        });
+
+        photoPrev.on('click', function(ev) {
+            ev.preventDefault();
+            photo.prev(arPhoto);
+        });
+
         getPicasa(function(data) {
-            console.log(data);
-            photo.show(data.feed.entry[1]);
+            photo.show(data);
+            init(data);
         });
 
     };
